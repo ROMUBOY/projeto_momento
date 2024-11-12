5cc480de7db1a053ca651b8565b3cabf7f08dc39
Romualdo "Romuboy" Santos <romualdoosfilho94@gmail.com>
2024-11-12 15:17:22 -0300

    folder organizations and ship/junk prototype sprite

--- junk.gd
+++
@@ -1,4 +0,0 @@
-extends Sprite2D
-
-func _init() -> void:
-	frame = randi_range(0, 100)
--- world.gd
+++ world.gd
@@ -1,10 +1,11 @@
 extends Node2D
 
-const Player = preload("res://player.tscn")
-const Exit = preload("res://exit.tscn")
-const Junk = preload("res://junk.tscn")
+const Player = preload("res://Player/player.tscn")
+const Exit = preload("res://LevelExit/exit.tscn")
+const Junk = preload("res://Junk/junk.tscn")
 
 var borders = Rect2(1, 1, 28, 21)
+var player_start_position : Vector2
 
 @onready var tile_map = $TileMap
 
@@ -20,11 +21,12 @@ func generate_level():
 	var player = Player.instantiate()
 	add_child(player)
 	player.position = map.front() * 32
+	player_start_position = player.position
 	
 	var exit = Exit.instantiate()
 	add_child(exit)
 	#exit.position = walker.get_end_room().position * 32
-	exit.position = map.front() * 25
+	exit.position = walker.rooms[1].position * 32
 	exit.connect("leaving_level", Callable(self, "reload_level"))
 	
 	walker.queue_free()
@@ -40,4 +42,14 @@ func generate_level():
 			junk.position = room.position * 32
 
 func reload_level():
+	get_tree().get_root().get_node("World" +"/"+ "Player").queue_free()
+	$CanvasLayer.show()
+
+func _on_new_area_button_pressed() -> void:
 	get_tree().reload_current_scene()
+
+func _on_revisit_area_button_pressed() -> void:
+	var player = Player.instantiate()
+	add_child(player)
+	player.position = player_start_position
+	$CanvasLayer.hide()
